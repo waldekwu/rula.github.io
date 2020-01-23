eb5dd5e1c04ca485a889e19aed05c2a67ab81126
waldekwu <34169309+waldekwu@users.noreply.github.com>
2020-01-23 16:59:51 +0000

    yyy

--- js/main.js
+++ js/main.js
@@ -912,3 +912,41 @@ function getPDF() {
 // }
 
 }
+
+
+
+function resultsModal(){
+
+let modal = document.getElementById("results-modal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("footer-text");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+  // let itm = document.getElementById("step-1");
+  // let cln = itm.cloneNode(true);
+  // document.getElementById("modal-body").appendChild(cln);
+
+
+
+}
